214364be3b8bdf2e8e45eeafe005ee69515deacc
nonni1234 <nonni1234567@gmail.com>
2021-04-30 14:46:44 +0000

    grunn app komið

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # FunkyWeatherApp
 App that shows you the weather in a cool user friendly way
 
-## mun sýna sólarupprás og sólsetur
+## Hugmyndir/aðrir möguleikar
+- Sýnir hvenær næsta sólarupprás/sólsetur er (fer eftir hvort er næst)
+- Ef við gerum þetta er hægt að gera eitthvað sem sýnir hvernig veðrið mun vera á eftir
+
--- app.js
+++ app.js
@@ -1,11 +1,88 @@
 const apikey = "c0bd32b3ab26640600d6f7e7219aa84d";
 
+const icons = {
+    "50d": "./ico"
+};
+
+function getIcon(iconid) {
+    const id = parseInt(iconid);
+    if (id == NaN) console.log("id failed");
+    let bg = "red";
+    let nafn;
+    let source;
+    let night;
+    if (id >= 200 && id <= 232) {
+        // Thunderstorm
+        nafn = "clouds";
+        bg = "color";
+    }
+    else if (id >= 300 && id <= 321) {
+        // Drizzle+
+        nafn = ""
+        bg = ""
+    }
+    else if (id >= 500 && id <= 531) {
+        // Rain
+        nafn = ""
+        bg = ""
+    }
+    else if (id >= 701 && id <= 781) {
+        // Atmosphere
+        nafn = "clouds";
+        bg = "grey"; 
+        
+    }
+    
+    else if (id >= 800 && id <= 804) {
+        switch (id) {
+            case 800:
+                nafn = "sun";
+                bg = "yellow";
+                break;
+            case 801:
+                nafn = "half-cloudy";
+                bg = "grey";
+                break;
+            case 802:
+            case 803:
+                nafn = "cloud";
+                bg = "grey";
+                break;
+            case 804:
+                nafn = "fullcloud";
+                bg = "grey";
+                break;
+        }
+    }
+
+    if (!night) {
+        source = `./icons/${nafn}.svg`;
+    } else {
+        // night source
+        // kristinn er inspiration
+        try {
+        source = `./icons/${nafn+"n"}.svg`;
+        } catch {
+            source = `./icons/${nafn}.svg`;
+        }
+    }
+    
+    console.log(source);
+    return {source,bg};
+}
+
+function Weather(weather) {
+    document.querySelector("#heat").textContent = Math.round(weather.main.temp)+"°";
+}
+
 function getWeather(lat,lon) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${"metric"}`;
+    //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${"metric"}`;
+    const url = "./debugmanualedit.json";
     console.log("url: " + url);
     fetch(url).then((response) => {
         response.json().then((data) => {
-            printWeatherOfLocation(data);
+            Weather(data);
+            changeIcon(data.weather[0].id);
         })
     }).catch((err) => {console.log("Fetch error: "+err )})
 };
@@ -18,9 +95,21 @@ function getLocation() {
     } else {
       console.log("Geolocation is not supported by this browser :(");
     }
-}
-function printWeatherOfLocation(weather) {
-    console.log(weather);
+}// pussy
+
+function changeIcon(id) {
+    const { source, bg } = getIcon(id);
+    console.log(id,source,bg);
+    const icon = document.querySelector("#icon");
+
+    try {
+    icon.src = source;
+    document.body.style.backgroundColor = bg;
+    } catch {
+        console.log("error with changing background, loading default values");
+        icon.src = "./icons/sun.svg";
+        document.body.style.backgroundColor = "cyan";
+    }
 }
 
-document.querySelector("button").addEventListener("click",getLocation);
+getLocation();
